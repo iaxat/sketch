595ec5959ab0638e8fdb50154a236e42c5f24bbd
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-26 04:28:29 -0500

    up

---
+++ sketch.py
@@ -0,0 +1,14 @@
+# Converting Image to sketch
+
+import cv2 # import open cv
+
+
+class Sketch():
+    def __init__(self) -> None:
+        get_image = input('Image Name Path: ')
+
+    # def read_image(self):
+
+
+
+get_sketch = Sketch()
