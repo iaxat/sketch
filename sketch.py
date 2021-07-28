846931f7e5abbf5317e1c50120ca6364d0906744
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-28 18:33:37 -0500

    up

--- sketch.py
+++ sketch.py
@@ -7,10 +7,22 @@ class Sketch():
     def read_image(self):
         get_image_file = input('Path or file name of the image: ')
         read_image = cv2.imread(get_image_file)
-        cv2.imshow('scren', read_image)
+        cv2.imshow('screen', read_image)
         cv2.waitKey(0)
 
-    
+        
+        # creating a gray image using cv2 library
+        gray_image = cv2.cvtColor(read_image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('new screen', gray_image)
+        cv2.waitKey(0)
+
+        # create inverted image from grayscale
+        inverted_image = 255 - gray_image
+        cv2.imshow('Inverted', inverted_image)
+        cv2.waitKey()
+
+        # Gaussian Function to blur image
+        
 
 
 
